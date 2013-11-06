e2103a3e82eeacfe4b0d0adbe0c2af01df1aa1e6
Alejandro Ballesteros <alexjabf@hotmail.com>
2013-11-05 23:56:05 -0600

    Cambios

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,8 +1,8 @@
 class Ip < ActiveRecord::Base
   attr_accessible :ip, :city, :state, :country, :zipcode
   
-  def to_param
-    "#{id}-#{city.parameterize}-#{state.parameterize}-#{country.parameterize}"
-  end
+  #def to_param
+  #  "#{id}-#{city.parameterize}-#{state.parameterize}-#{country.parameterize}"
+  #end
   
 end
